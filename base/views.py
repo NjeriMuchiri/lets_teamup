699c0940aea4ce5bfc7f52e6682e116d7d9ca8df
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-07-08 21:05:33 +0300

    added new templates

--- base/views.py
+++ base/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 
-rooms = [
-    {'id': 1, 'name': 'let us learn Python!'},
-    {'id': 2, 'name': 'Design with me'},
-    {'id': 3, 'name': 'Frontend developers'},
-]
-# Create your views here.
+#Create your views here.
 def home(request):
+    rooms = [
+        {'id': 1, 'name': 'let us learn Python!'},
+        {'id': 2, 'name': 'Design with me'},
+        {'id': 3, 'name': 'Frontend developers'},
+    ]
     context = {'rooms': rooms}
-    return render(request, 'home.html', context)
+    return render(request, 'base/home.html', context)
 
 def rooms(request):
     return render(request, 'room.html')
